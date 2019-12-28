720ac941c6af614d982ca7ab10fd6cdb331f1afa
rko2911 <ravikr.kumar50@gmail.com>
2019-12-28 17:25:54 +0530

    new changes are pushed to master branch

--- src/main/java/Basictest/CommonSteps.java
+++ src/main/java/Basictest/CommonSteps.java
@@ -1,11 +1,21 @@
 package Basictest;
 
+import java.util.List;
+
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.security.Credentials;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import Utility.Util;
 
 public class CommonSteps extends BasicTest {
+	static WebDriverWait wait = new WebDriverWait(driver,20);
 
 	public static boolean enterValue(String propvalue) throws InterruptedException {
 		boolean status=false;
@@ -99,6 +109,47 @@ public static boolean commonClick(String propvalue) throws InterruptedException 
 	}
 	return status;
 	}
+
+public static void selectFromDropdown() {
+	
+	WebElement element = driver.findElement(By.id("id"));
+	Select sel =new Select(element);
+	List<WebElement> list=sel.getOptions();
+	for(WebElement we: list) {
+		String st=we.getText();
+		if(st.equalsIgnoreCase("value")) {
+			sel.selectByValue(st);
+		}
+		
+	}
+	
+}
+
+public static void doubleClick() {
+	
+	WebElement we= driver.findElement(By.id("ID"));
+	Actions act = new Actions(driver);
+	act.doubleClick(we).perform();
+	
+	Action mouseHover= act.moveToElement(we).build();
+	mouseHover.perform();
+	
+	WebElement wes= driver.findElement(By.id("IDS"));
+	WebElement wed= driver.findElement(By.id("IDD"));
+	
+	Action dragndrop= act.dragAndDrop(wes, wed).build();
+	dragndrop.perform();
+	
+	
+	driver.switchTo().alert().setCredentials((Credentials) new UsernamePasswordCredentials("cheese",
+			    "secretGouda"));
+	
+	wait.until(ExpectedConditions.visibilityOf(we));
+	
+	
+	
+}
+
 }
 
 	
